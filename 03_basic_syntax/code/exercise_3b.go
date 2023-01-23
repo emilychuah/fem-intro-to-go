fe31eb9f3a43fff13ae2c26475a6dc1ae1d15f57
Emily Chuah <emily_earn@live.com>
2023-01-23 18:37:09 +1100

    Changed the var name for weather

--- 03_basic_syntax/code/exercise_3b.go
+++ 03_basic_syntax/code/exercise_3b.go
@@ -6,19 +6,19 @@ func main() {
 	var name string
 	var city string
 	var lengthOfStay int
-	var weatherBool bool
+	var weather bool
 
-	fmt.Print("What's your name? ")
+	fmt.Println("What's your name? ")
 	fmt.Scan(&name)
 
-	fmt.Print("What city do you live in? ")
+	fmt.Println("What city do you live in? ")
 	fmt.Scan(&city)
 
-	fmt.Print("How long have you lived there? ")
+	fmt.Println("How long have you lived there? ")
 	fmt.Scan(&lengthOfStay)
 
-	fmt.Print("Is the weather there nice? ")
-	fmt.Scan(&weatherBool)
+	fmt.Println("Is the weather there nice? (true/false) ")
+	fmt.Scan(&weather)
 
-	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t", name, city, lengthOfStay, weatherBool)
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t", name, city, lengthOfStay, weather)
 }
